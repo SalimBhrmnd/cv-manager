ab25faf24b89dbdb556937545df85ab37b038d06
SalimBhrmnd <salim.bahremand@gmail.com>
2024-01-14 17:22:30 +0330

    admindash

---
+++ AdminDash/src/layouts/MainLayout.vue
@@ -0,0 +1,111 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          My CV MANAGER
+        </q-toolbar-title>
+
+        <div>CV v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'آموزشی',
+    caption: 'َAytronic',
+    icon: 'school',
+    link: 'https://ay7.ir/'
+  },
+  {
+    title: 'گیتهاب من',
+    caption: 'github.com/SalimBahremand',
+    icon: 'code',
+    link: 'https://github.com/SalimBhrmnd'
+  },
+  {
+    title: 'تلگرام',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'واتس اپ',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Linkedin',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://www.linkedin.com/in/salim-bahremand-3a1308243/'
+  },
+  {
+    title: 'Facebook',
+    caption: 'فیس بوک',
+    icon: 'public',
+    link: 'https://facebook.salim.b'
+  },
+
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ AdminDash/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="My logo"
+      src="~assets/logo-color.svg"
+      style="width: 300px; height: 300px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
