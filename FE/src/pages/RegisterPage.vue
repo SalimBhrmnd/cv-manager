06ea90b10ddb0bb263c7bbd1395e6418695a1ef1
SalimBhrmnd <salim.bahremand@gmail.com>
2024-01-17 17:35:26 +0330

    register

---
+++ FE/src/pages/RegisterPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="">
+    <q-input aria-placeholder="username" label="User Name" rounded outlined class="q-mt-lg"></q-input>
+    v-model="username"
+    <q-input aria-placeholder="password" label="Password" rounded outlined class="q-mt-lg"></q-input>
+    <q-btn class="full-width" q-mt-md q-py-sm color="primary" rounded>Register</q-btn>
+    {{ username }}
+    <img
+      alt="My logo"
+      src="~assets/logo-color.svg"
+      style="width: 300px; height: 300px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup (){
+    const username = ref('')
+    return {
+      username
+    }
+  }
+})
+</script>
