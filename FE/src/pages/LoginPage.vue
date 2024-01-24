11dee41f1c1d2226ffb38f59d52f0f9870c8a748
SalimBhrmnd <salim.bahremand@gmail.com>
2024-01-24 15:40:52 +0330

    login button

--- FE/src/pages/LoginPage.vue
+++ FE/src/pages/LoginPage.vue
@@ -1,6 +1,14 @@
 <template>
   <q-page padding>
     <!-- content -->
+    <div class="row justify-center">
+      <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6">
+        <q-input rounded outlined label="email" type="email" placeholder="someemail@gmail.com"/>
+        <q-input rounded outlined label="password" type="password" placeholder="password"/>
+        <q-btn rounded label="Login"></q-btn>
+      </div>
+
+    </div>
   </q-page>
 </template>
 
